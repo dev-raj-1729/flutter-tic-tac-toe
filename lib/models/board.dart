4494d362a64a38a8510b0a6b5d772618ef78b2bf
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-06-10 10:28:49 +0530

    added matrix in Board

--- lib/models/board.dart
+++ lib/models/board.dart
@@ -1,1 +1,13 @@
-class Board {}
+import 'package:flutter/cupertino.dart';
+
+class Board with ChangeNotifier {
+  late List<List<int>> _matrix;
+  Board() {
+    _matrix = new List<List<int>>.generate(3, (_) => List.filled(3, 0));
+    notifyListeners();
+    print(_matrix);
+  }
+  List<List<int>> get matrix {
+    return [..._matrix];
+  }
+}
