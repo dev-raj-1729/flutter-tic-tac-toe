7d40c1dee3228e9931ab3bb0adcd9d39e27a4533
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-06-11 14:35:57 +0530

    refactored PlayGrid

---
+++ lib/widgets/play_grid.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tic_tac_toe/models/board.dart';
+import 'package:flutter_tic_tac_toe/widgets/play_tile.dart';
+import 'package:provider/provider.dart';
+
+class PlayGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+      ),
+      itemBuilder: (context, index) {
+        final row = index ~/ 3;
+        final col = index % 3;
+        return PlayTile(row, col);
+      },
+      itemCount: 9,
+    );
+  }
+}
