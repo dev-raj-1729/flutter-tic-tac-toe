5369e53b3d15072712ef789f25e8dd72be1bd457
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-06-15 02:24:34 +0530

    changed variable names to follow guidelines

--- lib/models/computer.dart
+++ lib/models/computer.dart
@@ -1,22 +1,22 @@
 class Utils {
   static bool isBoardFull(List<int> board) {
     for (var val in board) {
-      if (val == Ai.EMPTY_SPACE) return false;
+      if (val == Ai.emptySpace) return false;
     }
 
     return true;
   }
 
   static bool isMoveLegal(List<int> board, int move) {
-    if (move < 0 || move >= board.length || board[move] != Ai.EMPTY_SPACE)
+    if (move < 0 || move >= board.length || board[move] != Ai.emptySpace)
       return false;
 
     return true;
   }
 
   static int evaluateBoard(List<int> board) {
-    for (var list in Ai.WIN_CONDITIONS_LIST) {
-      if (board[list[0]] != Ai.EMPTY_SPACE &&
+    for (var list in Ai.winConditionsList) {
+      if (board[list[0]] != Ai.emptySpace &&
           board[list[0]] == board[list[1]] &&
           board[list[0]] == board[list[2]]) {
         return board[list[0]];
@@ -24,10 +24,10 @@ class Utils {
     }
 
     if (isBoardFull(board)) {
-      return Ai.DRAW;
+      return Ai.draw;
     }
 
-    return Ai.NO_WINNERS_YET;
+    return Ai.noWinnersYet;
   }
 
   static int flipPlayer(int currentPlayer) {
@@ -36,19 +36,18 @@ class Utils {
 }
 
 class Ai {
-  static const int HUMAN = 1;
-  static const int AI_PLAYER = -1;
-  static const int NO_WINNERS_YET = 0;
-  static const int DRAW = 2;
+  static const int human = 1;
+  static const int aiPlayer = -1;
+  static const int noWinnersYet = 0;
+  static const int draw = 2;
 
-  static const int EMPTY_SPACE = 0;
-  static const SYMBOLS = {EMPTY_SPACE: "", HUMAN: "X", AI_PLAYER: "O"};
+  static const int emptySpace = 0;
 
-  static const int WIN_SCORE = 100;
-  static const int DRAW_SCORE = 0;
-  static const int LOSE_SCORE = -100;
+  static const int winScore = 100;
+  static const int drawScore = 0;
+  static const int loseScore = -100;
 
-  static const WIN_CONDITIONS_LIST = [
+  static const winConditionsList = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -66,12 +65,12 @@ class Ai {
   int _getBestScore(List<int> board, int currentPlayer) {
     int evaluation = Utils.evaluateBoard(board);
 
-    if (evaluation == currentPlayer) return WIN_SCORE;
+    if (evaluation == currentPlayer) return winScore;
 
-    if (evaluation == DRAW) return DRAW_SCORE;
+    if (evaluation == draw) return drawScore;
 
     if (evaluation == Utils.flipPlayer(currentPlayer)) {
-      return LOSE_SCORE;
+      return loseScore;
     }
 
     return _getBestMove(board, currentPlayer).score;
