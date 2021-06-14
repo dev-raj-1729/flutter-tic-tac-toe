b9e3f393e4d763ff98b7581fdf63a6b1540df765
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-06-14 16:54:27 +0530

    added play_form file

---
+++ lib/widgets/play_form.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PlayForm extends StatefulWidget {
+  @override
+  _PlayFormState createState() => _PlayFormState();
+}
+
+class _PlayFormState extends State<PlayForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Play Form');
+  }
+}
