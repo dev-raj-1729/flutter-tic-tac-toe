7a7bc3e42a900fb1c60ce5a50ed2f66867280ea4
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-06-14 20:56:18 +0530

    fixed logscreen update issue

--- lib/screens/logscreen.dart
+++ lib/screens/logscreen.dart
@@ -8,42 +8,54 @@ class LogScreen extends StatelessWidget {
   const LogScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final logProvider = Provider.of<Log>(context);
+    // final logProvider = Provider.of<Log>(context,listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('History'),
       ),
-      body: ListView.builder(
-        itemCount: logProvider.logs.length,
-        itemBuilder: (context, index) {
-          final log = logProvider.logs[index];
-          return Dismissible(
-            onDismissed: (_) {
-              logProvider.removeItemById(log.id);
-            },
-            key: Key(log.id),
-            direction: DismissDirection.startToEnd,
-            background: Container(
-              padding: EdgeInsets.only(left: 20),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5), color: Colors.red),
-              child: Icon(
-                Icons.delete,
-                color: Colors.white,
-                size: 30,
+      body: FutureBuilder<void>(
+          future: Provider.of<Log>(context, listen: false).fetchAndUpdate(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return Consumer<Log>(
+              builder: (context, logProvider, _) => ListView.builder(
+                itemCount: logProvider.logs.length,
+                itemBuilder: (context, index) {
+                  final log = logProvider.logs[index];
+                  return Dismissible(
+                    onDismissed: (_) {
+                      logProvider.removeItemById(log.id);
+                    },
+                    key: Key(log.id),
+                    direction: DismissDirection.startToEnd,
+                    background: Container(
+                      padding: EdgeInsets.only(left: 20),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.red),
+                      child: Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      alignment: Alignment.centerLeft,
+                    ),
+                    child: Card(
+                      child: ListTile(
+                        title: Text('${log.winner} won against ${log.loser}'),
+                        subtitle: Text(
+                            'Date : ${DateFormat('dd-MM-yyyy').format(log.date)}'),
+                      ),
+                    ),
+                  );
+                },
               ),
-              alignment: Alignment.centerLeft,
-            ),
-            child: Card(
-              child: ListTile(
-                title: Text('${log.winner} won against ${log.loser}'),
-                subtitle:
-                    Text('Date : ${DateFormat('dd-MM-yyyy').format(log.date)}'),
-              ),
-            ),
-          );
-        },
-      ),
+            );
+          }),
     );
   }
 }
